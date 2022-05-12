85f14b250178e9d73638552bebd3cec14403695e
Zach Lefler <leaving.valhalla@gmail.com>
2022-05-11 17:31:31 -0700

    commit tree e0238e5e83d31de7c5f4fac60a5948bea773fbb3

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,40 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(string)
+    nodes_to_visit = [@root]
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      return node if node[:id] == string
+      nodes_to_visit = node[:children] + nodes_to_visit
+    end
+  end
+
+      
+
+
 end
+
+# def breadth_first_traversal(node)
+#   result = []
+#   nodes_to_visit = [node]
+#   while nodes_to_visit.length > 0
+#     node = nodes_to_visit.shift
+#     result.push(node[:value])
+#     nodes_to_visit += node[:children]
+#   end
+#   result
+# end
+
+# def depth_first_traversal(node)
+#   result = []
+#   nodes_to_visit = [node]
+#   while nodes_to_visit.length > 0
+#     node = nodes_to_visit.shift
+#     result.push(node[:value])
+#     # nodes_to_visit.unshift(node[:children])
+#     nodes_to_visit = node[:children] + nodes_to_visit
+#   end
+#   result
+# end
